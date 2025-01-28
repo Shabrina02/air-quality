8e4c4f5b9398841195b7e1eb2cb5db5f360bc64d
shabr <77773526+Shabrina02@users.noreply.github.com>
2025-01-28 15:13:23 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 # Load dataset
 st.title("Air Quality Dashboard - Guanyuan Station")
-all_data = pd.read_csv("C:/Users/Shabrina/Documents/[03] Career path/202411 - IDCamp Dicoding/Latihan/all_data.csv")
+all_data = pd.read_csv("all_data.csv")
 all_data['timestamp'] = pd.to_datetime(all_data['timestamp'])
 
 sorted_months = list(range(1, 13))  # Proper month order
